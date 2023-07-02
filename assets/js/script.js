0743509e9aca7db82ac5e6ccffff9889a4a2cd41
Nigel D. Watkins <nigeldwatkins@gmail.com>
2023-07-02 17:48:42 -0400

    finished my index and css for now, starting working on getting references from html elements and setting up my arrays

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// Getting references to HTML elements
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+const cityName = document.getElementById("cityname");
+const cityHistory = document.getElementById("city-history");
+
+const indexs = [0, 7, 15, 23, 31, 39];
+
+const cityday = [
+    document.getElementById("cityday"),
+    document.getElementById("cityday1"),
+    document.getElementById("cityday2"),
+    document.getElementById("cityday3"),
+    document.getElementById("cityday4"),
+    document.getElementById("cityday5"),
+]; 
+
+const temperature = [
+    document.getElementById("temp"),
+    document.getElementById("temp1"),
+    document.getElementById("temp2"),
+    document.getElementById("temp3"),
+    document.getElementById("temp4"),
+    document.getElementById("temp5"),
+];
+
+const humid = [
+    document.getElementById("humid"),
+    document.getElementById("humid1"),
+    document.getElementById("humid2"),
+    document.getElementById("humid3"),
+    document.getElementById("humid4"),
+    document.getElementById("humid5"),
+];
+
+const wind = [
+    document.getElementById("wind"),
+    document.getElementById("wind1"),
+    document.getElementById("wind2"),
+    document.getElementById("wind3"),
+    document.getElementById("wind4"),
+    document.getElementById("wind5"),
+];
